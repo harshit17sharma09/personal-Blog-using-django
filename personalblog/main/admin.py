69dcbe15a3155752772840adee8da79216949b3c
harshit17sharma09 <harshit17sharma09@gmail.com>
2018-06-14 13:49:40 +0530

    Template tags

--- personalblog/main/admin.py
+++ personalblog/main/admin.py
@@ -1,15 +1,11 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
-from .models import Post,Comment
-# Register your models here.
+from .models import Post, Comment
+
 
 @admin.register(Post)
-#customizing the admin panel
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title','slug','author','publish','status')
-
+    list_display = ('title', 'slug', 'author', 'publish',   
+                       'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
     prepopulated_fields = {'slug': ('title',)}
@@ -17,9 +13,9 @@ class PostAdmin(admin.ModelAdmin):
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
 
-@admin.register(Comment)
-class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
 
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'email', 'post', 'created', 'active') 
+    list_filter = ('active', 'created', 'updated') 
+    search_fields = ('name', 'email', 'body') 
--- personalblog/main/urls.py
+++ personalblog/main/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import url
-from . import views
+from . import views 
+
 
 app_name = 'main'
-
 urlpatterns = [
     # post views
-    # url('', views.post_list, name='post_list'),
-    url('tag/<slug:tag_slug>/',views.post_list, name='post_list_by_tag'),
-    # url('', views.PostListView.as_view(), name='post_list'),
+    url('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
     url('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,
-    name='post_detail'),
-]
+         name='post_detail'),
+    url('<int:post_id>/share/',
+         views.post_share, name='post_share'),
+    url('tag/<slug:tag_slug>/',
+         views.post_list, name='post_list_by_tag'),
+] 
